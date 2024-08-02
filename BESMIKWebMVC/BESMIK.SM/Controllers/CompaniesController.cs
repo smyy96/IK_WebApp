2e291970b4040cfdbb0682e3ebb2d656297ae50f
Eyşan Tekinsoy <eysantekinsoy@hotmail.com>
2024-08-02 15:08:18 +0300

    Admin Panel düzenlendi

---
+++ BESMIKWebMVC/BESMIK.SM/Controllers/CompaniesController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BESMIK.SM.Controllers
+{
+    public class CompaniesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        public IActionResult Details() 
+        {
+            return View();
+        }
+        public IActionResult Edit() 
+        {
+            return View();
+        }
+        public IActionResult Delete() 
+        {
+            return View();
+        }
+    }
+}
