4636aec7e8ffe7597f965627c1d5a2da5f272f34
Eyşan Tekinsoy <eysantekinsoy@hotmail.com>
2024-08-13 10:12:44 +0300

    Permission Controller eklendi.

---
+++ BESMIKWebMVC/BESMIK.SM/Areas/Personal/Controllers/PermissionController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BESMIK.SM.Areas.Personal.Controllers
+{
+    public class PermissionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
