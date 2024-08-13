2f4c6113a5ba29f69f40fd08539eb9f58070f8b5
smyy.cskn@hotmail.com <Sumeyye12.Sumeyye>
2024-08-13 09:45:12 +0300

    izinler tablosu

--- BESMIKWebApp/NTier/BESMIK.Entities/Concrete/Permission.cs
+++ BESMIKWebApp/NTier/BESMIK.Entities/Concrete/Permission.cs
@@ -16,7 +16,11 @@ namespace BESMIK.Entities.Concrete
         public string OffDaysNumbers { get; set; }
         public PermissionType PermissionType { get; set; }
         public PermissionStatus PermissionStatus { get; set; }
-        public ICollection<AppUser> AppUsers { get; set; }
+
+
+
+        public int AppUserId { get; set; }
+        public AppUser AppUser { get; set; }
 
     }
 }
