adae368053380557483d8e247909728ca2fe5060
Bengü Ürgen Demir <benguurgen@gmail.com>
2024-08-14 09:41:30 +0300

    eklemeler yapıldı.

--- BESMIKWebMVC/BESMIK.SM/Areas/Personal/Controllers/PermissionController.cs
+++ BESMIKWebMVC/BESMIK.SM/Areas/Personal/Controllers/PermissionController.cs
@@ -1,4 +1,4 @@
-﻿using BESMIK.ViewModel.Company;
+﻿using BESMIK.ViewModel.Permission;
 using FluentValidation;
 using Microsoft.AspNetCore.Mvc;
 using System.ComponentModel.DataAnnotations;
@@ -7,38 +7,41 @@ namespace BESMIK.SM.Areas.Personal.Controllers
 {
     public class PermissionController : Controller
     {
-        public class CompaniesController : Controller
+        private HttpClient _httpClient;
+        private IValidator<PermissionViewModel> _validator;
+
+        public PermissionController(HttpClient httpClient, IValidator<PermissionViewModel> validator)
         {
-            private HttpClient _httpClient;
-            private IValidator<CompanyViewModel> _validator;
-
-            public CompaniesController(HttpClient httpClient, IValidator<CompanyViewModel> validator)
-            {
-                _httpClient = httpClient;
-                _validator = validator;
-            }
-
-            [HttpGet]
-            public async Task<IActionResult> PermissionList()
-            {
-                var companies = await _httpClient.GetFromJsonAsync<List<CompanyViewModel>>("https://localhost:7136/api/Company/CompanyList");
-                return View(companies);
-            }
-
-
-            public async Task<IActionResult> PermissonAdd()
-            {
-                return View();
-            }
-
-            [HttpPost]
-            public async Task<IActionResult> PermissionAdd(CompanyViewModel model)
-            {
-                return View();
-            }
-
-
-
+            _httpClient = httpClient;
+            _validator = validator;
         }
+
+        [HttpGet]
+        public async Task<IActionResult> PermissionList()
+        {
+            var companies = await _httpClient.GetFromJsonAsync<List<PermissionViewModel>>("https://localhost:7136/api/Company/CompanyList");
+            return View(companies);
+        }
+
+
+        public async Task<IActionResult> PermissonAdd()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PermissionAdd(PermissionViewModel model)
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> PermissionDelete()
+        {
+            return View();
+        }
+
+
+
     }
 }
+
