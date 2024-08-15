f700c845cfdd01db7e2ebf49b518c7e94f42a7a3
mehmetcagri39 <mehmetcagri.celenk@bilgeadamboost.com>
2024-08-15 13:47:10 +0300

    Avans MVC kısmı yapıldı.

--- BESMIKWebApp/BESMIK.API/Controllers/AdvanceController.cs
+++ BESMIKWebApp/BESMIK.API/Controllers/AdvanceController.cs
@@ -1,5 +1,7 @@
 ﻿using BESMIK.BLL.Managers.Concrete;
+using BESMIK.Entities.Concrete;
 using BESMIK.ViewModel.Advance;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BESMIK.API.Controllers
@@ -9,10 +11,14 @@ namespace BESMIK.API.Controllers
     public class AdvanceController : ControllerBase
     {
         private readonly AdvanceManager _advanceManager;
+        private readonly UserManager<AppUser> _userManager;
 
-        public AdvanceController(AdvanceManager advanceManager)
+
+        public AdvanceController(AdvanceManager advanceManager, UserManager<AppUser> userManager)
         {
             _advanceManager = advanceManager;
+            _userManager = userManager;
+
         }
 
         //*--------------------------------------------------------------------------------------------------------------------------------------------*
@@ -26,17 +32,49 @@ namespace BESMIK.API.Controllers
                 return BadRequest(ModelState);
             }
 
+            // Assuming the advance is associated with the AppUserId correctly in the Add method of AdvanceManager
             _advanceManager.Add(model);
             return Ok(model);
         }
 
+
         //*--------------------------------------------------------------------------------------------------------------------------------------------*
         //Avans Taleplerini Listeleme
-        [HttpGet("AdvancesList")]
-        public ActionResult<IEnumerable<AdvanceViewModel>> AdvanceGet()
+        [HttpGet("AdvancesList/{userId}")]
+        public ActionResult<IEnumerable<AdvanceViewModel>> AdvanceGet(int userId)
         {
-            return Ok(_advanceManager.GetAll());
+            var advances = _advanceManager.GetAll()
+                                          .Where(a => a.AppUserId == userId)
+                                          .ToList();
+            return Ok(advances);
         }
-        
+
+
+        [HttpGet("GetUserInfo/{name}")]
+        public async Task<IActionResult> GetUserInfo(string name)
+        {
+            var user = await _userManager.FindByNameAsync(name);
+            //await _userManager.
+            if (user == null)
+            {
+                return NotFound("Kullanıcı bulunamadı.");
+            }
+            return Ok(user);
+
+        }
+
+        [HttpGet("GetUser/{id}")]
+        public async Task<IActionResult> GetUser(int id)
+        {
+            var user = await _userManager.FindByIdAsync(id.ToString());
+
+            if (user == null)
+            {
+                return NotFound("Kullanıcı bulunamadı.");
+            }
+
+            return Ok(user);
+        }
+
     }
 }
