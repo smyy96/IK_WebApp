6323b01f614fbee6090dc8f43e98b05aeee68e51
Bengü Ürgen Demir <benguurgen@gmail.com>
2024-08-17 00:49:18 +0300

    İzinler için silme ve güncelleme yapıldı.

--- BESMIKWebMVC/BESMIK.SM/Areas/Personal/Controllers/PermissionController.cs
+++ BESMIKWebMVC/BESMIK.SM/Areas/Personal/Controllers/PermissionController.cs
@@ -1,4 +1,5 @@
 ﻿using BESMIK.Common;
+using BESMIK.ViewModel.Advance;
 using BESMIK.ViewModel.AppUser;
 using BESMIK.ViewModel.Permission;
 using BESMIK.ViewModel.Spending;
@@ -9,6 +10,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Security.Claims;
 
 namespace BESMIK.SM.Areas.Personal.Controllers
 {
@@ -105,6 +107,100 @@ namespace BESMIK.SM.Areas.Personal.Controllers
                 return View(model);
             }
         }
+        [HttpPost]
+        public async Task<IActionResult> PermissionDelete(int id)
+        {
+            try
+            {
+                var response = await _httpClient.DeleteAsync($"https://localhost:7136/api/Permission/PermissionDelete{id}");
+                if (response.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("PermissionList");
+                }
+                else
+                {
+                    ModelState.AddModelError("ApiError", "Avans silinemedi. Lütfen tekrar deneyin.");
+                    return RedirectToAction("PermissionList");
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("GeneralException", ex.Message);
+                return RedirectToAction("PermissionList");
+            }
+        }
+        [HttpGet]
+        public async Task<IActionResult> PermissionEdit(int id)
+        {
+            try
+            {
+                var response = await _httpClient.GetAsync($"https://localhost:7136/api/Permission/GetPermission/{id}");
+
+                if (response.StatusCode == System.Net.HttpStatusCode.NotFound)
+                {
+                    TempData["ErrorMessage"] = "İzin bulunamadı.";
+                    return RedirectToAction("PermissionList");
+                }
+
+                if (response.StatusCode == System.Net.HttpStatusCode.Forbidden)
+                {
+                    TempData["ErrorMessage"] = "İzin talebi onay bekliyor değilse düzenlenemez.";
+                    return RedirectToAction("PermissionList");
+                }
+                response.EnsureSuccessStatusCode();
+
+
+                var permission = await response.Content.ReadFromJsonAsync<PermissionViewModel>();
+            
+                return View(permission);
+            }
+            catch (HttpRequestException ex)
+            {
+                
+                TempData["ErrorMessage"] = "API çağrısında bir hata oluştu. " + ex.Message;
+                return RedirectToAction("PermissionList");
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PermissionEdit(PermissionViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var user = HttpContext.User.Identity.Name;
+            var request = await _httpClient.GetFromJsonAsync<AppUserViewModel>($"https://localhost:7136/api/AppUser/GetUserInfo/{user}");
+            if (request == null)
+            {
+                ModelState.AddModelError(string.Empty, "Kullanıcı bilgileri alınamadı.");
+                return View(model);
+            }
+
+            model.AppUserId = (int)request.Id;
+
+            // Tarih hesaplaması
+            DateOnly start = model.PermissionStartDate;
+            DateOnly end = model.PermissionEndDate;
+            int offDaysNumbers = end.DayNumber - start.DayNumber; // DateOnly kullanarak gün farkını hesaplayın.
+            model.OffDaysNumbers = offDaysNumbers.ToString();
+            model.PermissionResponseDate = null;
+            model.PermissionStatus = PermissionStatus.OnayBekliyor;
+            model.PermissionRequestDate = DateOnly.FromDateTime(DateTime.UtcNow);
+
+            var response = await _httpClient.PutAsJsonAsync($"https://localhost:7136/api/Permission/PermissionEdit/{model.Id}", model);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("PermissionList");
+            }
+
+            ModelState.AddModelError("", "İzin güncelleme başarısız.");
+            return View(model);
+
+        }
+
 
         [HttpGet]
         public IActionResult GetPermissionType()
