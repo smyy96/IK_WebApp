71c6931740bf28cb3a360ca4f1d39d1028338184
Bengü Ürgen Demir <benguurgen@gmail.com>
2024-08-19 10:04:24 +0300

    Layoutlar eklendi.

---
+++ BESMIKWebMVC/BESMIK.SM/Areas/CompanyManager/Controllers/CompanyManagerHomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BESMIK.SM.Areas.CompanyManager.Controllers
+{
+    public class CompanyManagerHomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
