b4d220ec5979458fe7868581d973c87053cebf81
mehmetcagri39 <mehmetcagri.celenk@bilgeadamboost.com>
2024-08-20 15:29:36 +0300

    Artık şirket yöneticileri avans taleplerine cevap verebiliyor.

---
+++ BESMIKWebApp/BESMIK.API/Controllers/AdvanceManagementController.cs
@@ -0,0 +1,58 @@
+﻿using BESMIK.BLL.Managers.Concrete;
+using BESMIK.Entities.Concrete;
+using BESMIK.ViewModel.Advance;
+using BESMIK.ViewModel.Permission;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BESMIK.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AdvanceManagementController : Controller
+    {
+        private readonly AdvanceManager _advanceManager;
+
+
+        public AdvanceManagementController(AdvanceManager advanceManager, UserManager<AppUser> userManager)
+        {
+            _advanceManager = advanceManager;
+
+        }
+
+        [HttpPut("EditAdvance/{id}")]
+        public IActionResult EditAdvance(int id, [FromBody] AdvanceViewModel guncellenmisAvans)
+        {
+            var varolanAvans = _advanceManager.Get(id);
+
+            if (varolanAvans == null)
+                return NotFound();
+
+            if (varolanAvans.ApprovalStatus != guncellenmisAvans.ApprovalStatus)
+            {
+                varolanAvans.ApprovalStatus = guncellenmisAvans.ApprovalStatus;
+                varolanAvans.AdvanceResponseDate = DateOnly.FromDateTime(DateTime.Now); 
+            }
+
+            varolanAvans.Description = guncellenmisAvans.Description;
+            varolanAvans.Amount = guncellenmisAvans.Amount;
+            varolanAvans.Currency = guncellenmisAvans.Currency;
+            varolanAvans.AdvanceType = guncellenmisAvans.AdvanceType;
+            varolanAvans.AdvanceRequestDate = guncellenmisAvans.AdvanceRequestDate;
+
+            _advanceManager.Update(varolanAvans);
+
+            return Ok("Avans talebi başarıyla güncellendi.");
+        }   
+
+        [HttpGet("AdvanceListByCompanyId/{companyId}")]
+        public ActionResult<IEnumerable<PermissionViewModel>> GetAdvanceListWithCompany(int companyId)
+        {
+            var advances = _advanceManager.GetAll()
+                                                       .Where(p => p.AppUser.CompanyId == companyId)
+                                                       .ToList();
+
+            return Ok(advances);
+        }
+    }
+}
---
+++ BESMIKWebMVC/BESMIK.SM/Areas/CompanyManager/Controllers/AdvanceManagementController.cs
@@ -0,0 +1,88 @@
+﻿using BESMIK.Common;
+using BESMIK.ViewModel.Advance;
+using BESMIK.ViewModel.AppUser;
+using BESMIK.ViewModel.Company;
+using BESMIK.ViewModel.Permission;
+using FluentValidation;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.Design;
+using System.Net.Http;
+using System.Security.Claims;
+
+namespace BESMIK.SM.Areas.CompanyManager.Controllers
+{
+    [Area("CompanyManager")]
+    [Authorize(Roles = "Sirket Yoneticisi")]
+    public class AdvanceManagementController : Controller
+    {
+
+        private HttpClient _httpClient;
+        private IValidator<CompanyViewModel> _validator;
+
+        public AdvanceManagementController(HttpClient httpClient, IValidator<CompanyViewModel> validator)
+        {
+            _httpClient = httpClient;
+            _validator = validator;
+        }
+
+        public async Task<IActionResult> AdvancesList()
+        {
+            var user = HttpContext.User.Identity.Name;
+            var request = await _httpClient.GetFromJsonAsync<AppUserViewModel>("https://localhost:7136/api/UserInfo/GetUserInfo/" + user);
+            var companyId = request.CompanyId;
+            var advancesList = await _httpClient.GetFromJsonAsync<IEnumerable<AdvanceViewModel>>("https://localhost:7136/api/AdvanceManagement/AdvanceListByCompanyId/" + companyId);
+            return View(advancesList);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> AdvanceEdit(int id)
+        {
+            var response = await _httpClient.GetAsync($"https://localhost:7136/api/Advance/GetAdvance/{id}");
+
+            if (response.StatusCode == System.Net.HttpStatusCode.NotFound)
+            {
+                TempData["ErrorMessage"] = "Avans bulunamadı.";
+                return RedirectToAction("AdvancesList");
+            }
+
+            response.EnsureSuccessStatusCode();
+
+            var advance = await response.Content.ReadFromJsonAsync<AdvanceViewModel>();
+
+            return View(advance);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AdvanceEdit(AdvanceViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var response = await _httpClient.PutAsJsonAsync($"https://localhost:7136/api/AdvanceManagement/EditAdvance/{model.Id}", model);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("AdvancesList");
+            }
+
+            ModelState.AddModelError("", "Avans güncelleme başarısız.");
+            return View(model);
+        }
+
+
+        [HttpGet]
+        public IActionResult GetAdvanceApprovalStatus()
+        {
+            var approvalStatus = Enum.GetValues(typeof(AdvanceApprovalStatus))
+                          .Cast<AdvanceApprovalStatus>()
+                          .Select(d => new { Value = ((int)d).ToString(), Text = d.ToString() })
+                          .ToList();
+
+            return Json(approvalStatus);
+        }
+
+    }
+}
